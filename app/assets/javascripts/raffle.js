7fde2bc5ec32688b51635eb668b656c55daccd29
Joseph Cabral <joe1363_2001@yahoo.com>
2015-12-27 17:47:09 -0800

    draw functionality

--- app/assets/javascripts/raffle.js
+++ app/assets/javascripts/raffle.js
@@ -1,19 +1,52 @@
 $(document).ready(function() {
+  var pool = []
+
+  ////name submit////
   $('#nameSubmit').click(function() {
     event.preventDefault();
-
     var name = $('#nameInput').val();
     if (name != '') {
-      $('.area').prepend('<p>' + name + '</p>');
+      $('.area').prepend('<div class="inline"><p>' + name + '</p></div>');
+      pool.push(name);
+
+      $('#nameInput').val('');
+      $('#nameInput').focus();
     }
-    $('#nameInput').val('');
-    $('#nameInput').focus();
+
   })
 
+  ////reset////
   $('#reset').click(function() {
     $('p').remove();
     $('#nameInput').val('');
     $('#nameInput').focus();
+    pool = []
+    $('#draw').prop('disabled', false);
+
   })
+
+  ////draw winner////
+  $('#draw').click(function() {
+    var rando;
+    var win;
+    console.log(rando);
+    console.log(pool);
+    rando = Math.floor(Math.random()*pool.length);
+    win = pool[rando];
+    winner = $("p:contains(" + win + ")");
+    winner.append('<span class="inline highlight"> WINNER</span>');
+    console.log(pool);
+    pool.splice(rando, 1);
+    console.log(pool);
+    console.log(rando);
+    if (pool.length == 0) {
+      $('#draw').prop('disabled', true);
+    }
+
+  })
+
+  function animation() {
+
+  }
 
 })
